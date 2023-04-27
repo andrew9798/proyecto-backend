a4e81239d4fad17093a5c09bc0460e30f46463fc
andrew9798 <andresganga1298@gmail.com>
2023-04-27 11:20:59 +0200

    Instalación de cors, express, mongoose y morgan y aplicación de cors en index.js

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ const port = process.env.PORT || 3000
  app.listen(port, () => {
      console.log(`escuchando en puerto ${port}`);
  })
+
+
+/*------------- Aplicación del Cors* -----------------*/
+
+ const whitelist = ["http://127.0.0.1:5000", "http://127.0.0.1:5001", "http://127.0.0.1:5002", "http://127.0.0.1:5003", "http://127.0.0.1:5004"]
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            logger.access.info("Acceso a la aplicación desde " + origin);
+            callback(null, true);
+        } else {
+            logger.error.fatal("Intento de acceso a la aplicación desde origen desautorizado: " + origin)
+            callback(null, false);
+        }
+    },
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+
+
+
+
+
