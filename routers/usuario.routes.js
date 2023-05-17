a5e7e8e6e6299ccbfd0bd8926a33da8f1ac567ef
andrew9798 <andresganga1298@gmail.com>
2023-05-17 10:25:54 +0200

    cambios en backend

--- routers/usuario.routes.js
+++ routers/usuario.routes.js
@@ -1,4 +1,20 @@
-const usuarioController = require("../controllers/");
+const usuarioController = require("../controllers/usuario.controller");
 const express = require("express");
 const router = express.Router();
 const anyadirMorgan = require("../middleware/morgan.mw");
+
+/**
+ * Ruta para recoger todos los usuarios
+ * Llama a la función get_usuario de usuario.Controller
+ * ej de ruta: http://localhost:3000/api/v1/comentarios
+ */
+router.get("/", anyadirMorgan, usuarioController.get_usuarios);
+
+/**
+ * Ruta para recoger un usuario por id de usuario definido en JSON
+ * Llama a la función get_usuario_by_id de usuario.Controller
+ * Ej de ruta: http://localhost:3000/api/v1/ejercicio/usuario {id_usuario a través de JSON}
+ */
+router.post("/usuario", anyadirMorgan, usuarioController.get_usuario_by_id);
+
+module.exports = router
